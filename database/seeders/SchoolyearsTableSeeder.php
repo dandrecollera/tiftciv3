8802a58a4a3195d0e7880bd76f61dc1ea6c43628
dandrecollera <dandrecollera@gmail.com>
2023-05-11 22:58:07 +0800

    school year

--- database/seeders/SchoolyearsTableSeeder.php
+++ database/seeders/SchoolyearsTableSeeder.php
@@ -14,12 +14,29 @@ class SchoolyearsTableSeeder extends Seeder
      */
     public function run()
     {
-
+        
 
         \DB::table('schoolyears')->delete();
-
-
-
-
+        
+        \DB::table('schoolyears')->insert(array (
+            0 => 
+            array (
+                'created_at' => '2023-05-10 21:00:20',
+                'id' => 1,
+                'school_year' => '2021-2022',
+                'status' => 'active',
+                'updated_at' => '2023-05-10 21:00:20',
+            ),
+            1 => 
+            array (
+                'created_at' => '2023-05-10 13:01:58',
+                'id' => 2,
+                'school_year' => '2022-2023',
+                'status' => 'active',
+                'updated_at' => '2023-05-10 13:01:58',
+            ),
+        ));
+        
+        
     }
 }
