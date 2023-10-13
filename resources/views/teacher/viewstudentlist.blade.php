42c9332a1439644bd10eefd22b1216bb92aab616
Jay Cruz <minjaexxii@gmail.com>
2023-10-13 15:06:25 +0800

    teacher

---
+++ resources/views/teacher/viewstudentlist.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Basic PDF</title>
+</head>
+<body>
+    <h1>My Basic PDF</h1>
+    <p>This is a simple PDF generated using Laravel and dompdf.</p>
+</body>
+</html>
